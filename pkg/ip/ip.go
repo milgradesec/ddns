a7b1877a74b383db06592cccf8be12ed0c03b054
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-02-10 03:38:19 +0100

    Update ip.go

--- pkg/ip/ip.go
+++ pkg/ip/ip.go
@@ -24,7 +24,7 @@ func GetIP() (string, error) {
 
 	req, err := http.NewRequest(http.MethodGet, "https://api.ipify.org/?format=json", nil)
 	if err != nil {
-		return "", fmt.Errorf("NewRequest to '%s' failed: %w", req.URL.String(), err)
+		return "", err
 	}
 
 	for i := 0; i < 5; i++ {
@@ -42,7 +42,7 @@ func GetIP() (string, error) {
 		var msg response
 		err = json.NewDecoder(resp.Body).Decode(&msg)
 		if err != nil {
-			return "", fmt.Errorf("failed to decode ipify response: %w", err)
+			return "", fmt.Errorf("failed to decode response: %w", err)
 		}
 
 		ip := net.ParseIP(msg.IP)
