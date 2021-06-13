61fe4db2a5bc076af5c382d40d9430edf12b7462
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-06-13 02:25:53 +0200

    Increase context timeout to 30s

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -82,7 +82,7 @@ func (m *Monitor) Run() {
 }
 
 func (m *Monitor) callProvider() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	if err := m.api.UpdateZone(ctx); err != nil {
