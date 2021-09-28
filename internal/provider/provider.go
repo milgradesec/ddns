e872919b803f5f4416b638aceaa0a1f9c009a2e7
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-09-28 20:40:58 +0200

    Update provider.go

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,8 +2,8 @@ package provider
 
 import "context"
 
-// DNSProvider interface represents an DNSProvider from a dns provider
-// able to query and modify records for a domain.
+// DNSProvider interface represents a dns provider able
+// to query and modify records for a domain.
 type DNSProvider interface {
 	Name() string
 	UpdateZone(ctx context.Context) error
